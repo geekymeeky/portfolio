7642c7dbec722716db60a47525ee024da0ce3093
Srijan Kr. Gupta <gupta.srijan94@gmail.com>
2022-01-18 17:50:00 +0530

    update: colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: "#342E37",
+        orange: "#F42C04",
+        yellow: "#F5E960",
+        blue: "#157A6E",
+        purple: "#8963BA",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
